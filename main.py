ff5a12d47f65c1d4484ba6a3ddecc0397834a723
SaVladimir <vova.savenko98@mail.ru>
2021-12-13 05:27:44 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,105 @@
+import os
+import logging
+import state_machine
+
+from dotenv import load_dotenv
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, Filters, MessageHandler, CallbackContext)
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)n - %(message)%', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+bot = state_machine.CheeseBot()
+
+
+def start(update: Update, context: CallbackContext):
+    bot.start()
+    message = 'Добро пожаловать.\nКакую вы хотите пиццу? Большую или маленькую?\n' \
+              'Если Вам нужна помощь введите команду /help.'
+    update.message.reply_text(message)
+    return
+
+
+def stop(update: Update, context: CallbackContext):
+    bot.stop()
+    message = 'Для заказа введите /start.'
+    update.message.reply_text(message)
+    return
+
+
+def help(update: Update, context: CallbackContext):
+    bot.help()
+    message = 'Для начала формирования заказа введите /start, чтобы отменить заказ /stop.\n' \
+              'Пока у нас есть только два варината пиццы: большая и маленькая.\n' \
+              'А оплату мы принимаем либо наличкой, либо картой.'
+    update.message.reply_text(message)
+    return
+
+
+def run(update: Update, context: CallbackContext):
+    text = update.message.text.lower()
+    sizes = [
+        'большую', 'маленькую', 'большая', 'маленькая'
+    ]
+    paying = [
+        'наличкой', 'картой', 'онлайн', 'наликом', 'курьеру'
+    ]
+    if bot.state == 'size_choose':
+        if text in sizes:
+            bot.pay()
+            bot.set_size(text)
+            message = 'Как вы будете платить?'
+            update.message.reply_text(message)
+        else:
+            update.message.reply_text('К сожалению я вас не понимаю.\n'
+                                      'Сейчас мы доставляем большую или маленькую пиццу.\n'
+                                      'Выберите из предложенных.')
+    elif bot.state == 'payment_method':
+        if text in paying:
+            bot.confirm()
+            bot.set_payment(text)
+            message = f'Вы хотите {bot.size} пиццу, оплата - {bot.payment}?'
+            update.message.reply_text(message)
+        else:
+            update.message.reply_text('Пожалуйста, выберите способ оплаты. Сейчас доступно два варианта оплаты:'
+                                      'картой или наличкой.')
+    elif bot.state == 'confirmation':
+        if text == 'да':
+            bot.done()
+            contact_button = KeyboardButton('Номер', request_contact=True)
+            location_button = KeyboardButton('Адрес', request_location=True)
+            my_keyboard = ReplyKeyboardMarkup([[contact_button, location_button]], resize_keyboard=True)
+            message = 'Спасибо за заказ.\n' \
+                      'Он отправлен на кухню, но для доставки нам нужны Ваши контакты.\n' \
+                      'Для еще одного заказа введите /start.'
+            update.message.reply_text(message, reply_markup=my_keyboard)
+        elif text == 'нет':
+            bot.done()
+            message = 'Так, видимо Вы еще не определились, давайте попробуем заново.\n' \
+                      'Введите команду /start'
+            update.message.reply_text(message)
+
+
+def main():
+    updater = Updater(TELEGRAM_TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('stop', stop))
+    dispatcher.add_handler(CommandHandler('help', help))
+    dispatcher.add_handler(MessageHandler(Filters.text, run))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ state_machine.py
@@ -0,0 +1,26 @@
+from transitions import Machine
+
+
+class CheeseBot(object):
+    states = ['chilling', 'size_choose', 'payment_method', 'confirmation']
+
+    def __init__(self):
+        self.size = None
+        self.payment = None
+
+        self.machine = Machine(model=self, states=CheeseBot.states, initial='chilling')
+
+        self.machine.add_transition(trigger='start', source='chilling', dest='size_choose')
+        self.machine.add_transition(trigger='pay', source='size_choose', dest='payment_method')
+        self.machine.add_transition(trigger='confirm', source='payment_method', dest='confirmation')
+        self.machine.add_transition(trigger='done', source='confirmation', dest='chilling')
+        self.machine.add_transition(trigger='stop', source='*', dest='chilling')
+        self.machine.add_transition(trigger='help', source='*', dest='chilling')
+
+    def set_size(self, size_choose):
+        self.size = size_choose
+        return self.size
+
+    def set_payment(self, payment_method):
+        self.payment = payment_method
+        return self.payment
